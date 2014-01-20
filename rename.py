e344e20ee8d739be12448ae9343c46080281dc8c
Csuganya <csuganyacse@gmail.com>
2014-01-20 20:24:21 +0530

    Create rename.py

---
+++ rename.py
@@ -0,0 +1,9 @@
+import os
+#os.rename("D:/sg/temp.txt","D:/sg/sample.txt")
+#os.remove("D:/sg/sample.txt")
+#print(os.getcwd())
+#os.mkdir("D:/sg/game.txt")
+print(os.getcwd())
+#os.chdir("D:/ss")
+print(os.getcwd())
+os.rmdir("D:/ss")
